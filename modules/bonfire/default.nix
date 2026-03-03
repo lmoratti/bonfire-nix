8cf3e155959ace4c01b684591f8195af604b81ca
lizzie moratti <moratti.lizzie@gmail.com>
2026-03-03 13:58:26 +0100

    Update default.nix

--- modules/bonfire/default.nix
+++ modules/bonfire/default.nix
@@ -431,7 +431,7 @@ in {
           (if cfg.mail-backend != null then { MAIL_BACKEND = "${cfg.mail-backend}"; } else {}) //
           (if cfg.mail-port != null then { MAIL_PORT = "${cfg.mail-port}"; } else {}) //
           (if cfg.mail-ssl then { MAIL_SSL = "${lib.trivial.boolToString cfg.mail-ssl}"; } else {}) //
-          (if cfg.zenodo-env then { ZENODO_ENV = "${cfg.zenodo-env}"; } else {}) //
+          (if cfg.zenodo-env != null then { ZENODO_ENV = "${cfg.zenodo-env}"; } else {}) //
           (if cfg.orcid-env then { ORCID_ENV = "${cfg.orcid-env}"; } else {}) //
           (if cfg.orcid-with-member-api then { ORCID_WITH_MEMBER_API = "${cfg.orcid-with-member-api}"; } else {});
         };
